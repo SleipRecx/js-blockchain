9df8d103a9674ed12306286a30a24eb08ba7d85b
Markus Andresen <markus.andresen94@gmail.com>
2019-06-08 21:34:13 +0200

    more validatiom

--- index.js
+++ index.js
@@ -8,19 +8,16 @@ class Transaction {
         this.to = to;
         this.amount = amount;
         this.timestamp = Date.now();
-      }
+    }
 
     calculateHash(){
         return SHA256(this.from + this.to + this.amount + this.timestamp).toString()
     }
 
-    sign(key){
-        if (key.getPublic('hex') !== this.from) {
-            throw new Error("You can only sign your own transactions.")
-        }
-        this.signature = key.sign(this.calculateHash(), 'base64').toDER('hex')
+    addSignature(signature){
+        this.signature = signature
     }
-
+ 
     isValid(){
         if (this.from === null) return true;
         if (!this.signature || this.signature.length === 0) return false;
@@ -44,17 +41,19 @@ class Block {
       return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
     }
 
-    hasValidTransactions() {
-        for (const tx of this.transactions) {
-          if (!tx.isValid()) {
-            return false;
-          }
+    isValid(){
+        if (this.hash !== this.calculateHash()){
+            return false
         }
+        for (const tx of this.transactions) {
+            if (!tx.isValid()) {
+              return false;
+            }
+          }
+        return true
+    }
 
-        return true;
-      }
-
-    mineBlock(difficulty) {
+    mine(difficulty) {
         while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
           this.nonce++;
           this.hash = this.calculateHash();
@@ -67,95 +66,76 @@ class Block {
 
 class Blockchain {
     constructor() {
-      this.chain = [new Block(Date.now(), [], 'genisis')]
+      this.chain = [new Block(Date.now(), [], '0000')]
       this.difficulty = 3
       this.pendingTransactions = []
-      this.miningReward = 50
+      this.miningReward = 25
     }
 
     addTransaction(transaction) {
          this.pendingTransactions.push(transaction);
     }
 
-    minePendingTransactions(adress){
+    mineBlock(adress){
         const rewardTransaction = new Transaction(null, adress, this.miningReward);
         this.pendingTransactions.push(rewardTransaction);
 
         let block = new Block(Date.now(), this.pendingTransactions, this.chain[this.chain.length - 1].hash);
-        const nonce = block.mineBlock(this.difficulty);
+        const nonce = block.mine(this.difficulty);
         this.chain.push(block);
-
-        /*
-        if (this.chain.length % 2016 === 0){
-            const twoWeeks = 1209600
-            const timeElapsed = Math.ceil((Date.now() - this.chain[this.chain.length - 2016].timestamp) / 1000)
-            console.log(timeElapsed)
-            this.difficulty = this.difficulty * twoWeeks / timeElapsed
-            console.log("Diff changed")
-            console.log(this.difficulty)
-        }
-        */
 
         this.pendingTransactions = [];
         return nonce
     }
 
-    getBalance(address){
-        let balance = 0;
 
-        for (const block of this.chain) {
-          for (const trans of block.transactions) {
-            if (trans.from === address) {
-              balance -= trans.amount;
-            }
-    
-            if (trans.to === address) {
-              balance += trans.amount;
-            }
-          }
-        }
-    
-        return balance;
-    }
     isValid(){
-        for (let i = 0; i < this.chain.length; i++) {
-            const block = this.chain[i]
-            
-            if (!block.hasValidTransactions()) {
+        for (let i = 1; i < this.chain.length; i++) {
+            const previous = this.chain[i-1]
+            const current = this.chain[i]
+
+            if (current.previousHash !== previous.hash){
+                return false
+            }
+
+            if (!current.isValid()) {
                 return false;
             }
-            if (block.hash !== block.calculateHash()) {
+            if (current.hash !== current.calculateHash()) {
                 return false;
             }
         }
         return true
     }
+
+    print(){
+        console.log(JSON.stringify(this.chain, null, 2))
+    }
 }
+
+class Wallet {
+    constructor(){
+        this.keyPair = ec.genKeyPair()   
+    }
+
+    getPublicKey(){
+        return this.keyPair.getPublic("hex")
+    }
+
+    sign(hash){
+        return this.keyPair.sign(hash, 'base64').toDER('hex')
+    }
+}
+
   
-  
-const alice = ec.genKeyPair();
-const alicePublic = alice.getPublic("hex")
-
-const bob = ec.genKeyPair();
-const bobPublic = bob.getPublic("hex")
-
-
+alice = new Wallet()
+bob = new Wallet()
 
 const blockchain = new Blockchain()
+const tx = new Transaction(alice.getPublicKey(), bob.getPublicKey(), 100)
+tx.addSignature(alice.sign(tx.calculateHash()))
 
+blockchain.addTransaction(tx)
+blockchain.mineBlock(alice.getPublicKey())
 
-
-blockchain.minePendingTransactions(alicePublic)
-
-//const tx = new Transaction(alicePublic, bobPublic, 100)
-//tx.sign(alice)
-
-//blockchain.addTransaction(tx)
-//blockchain.minePendingTransactions(alicePublic)
-
-console.log(blockchain.isValid())
-
-
-blockchain.chain[1].transactions[0].amount = 1000
-
-console.log(blockchain.isValid())
+blockchain.print()
